688cb206e02fd2caac0fcd4cdb91b1ae1b390d3b
Sean Ferguson <sean.ferguson@appfolio.com>
2013-04-19 18:57:44 -0700

    sf initial commit of rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpaceAlarp::Application.config.secret_token = 'e05a889e5846a9be4e240e6f2c59652a83d4604b9ea870ff1203e2d5a186f75ad067dd12a2bbbde7162e1f5736a0fe56fbd3271399555f728edc2344aadba98e'
