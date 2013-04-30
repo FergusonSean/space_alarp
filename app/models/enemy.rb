30148ea824a9e9962bf244084be0036830cda867
Sean Ferguson <sean.ferguson@appfolio.com>
2013-04-29 21:54:22 -0700

    stuff

--- app/models/enemy.rb
+++ app/models/enemy.rb
@@ -3,4 +3,21 @@ class Enemy < ActiveRecord::Base
   has_many :enemy_actions
   belongs_to :sector
   accepts_nested_attributes_for :enemy_actions
+  
+  def damage!(amount)
+    self.current_shield_strength -= amount
+    
+    if(current_shield_strength < 0)
+      self.health += current_shield_strength
+      self.current_shield_strength = 0
+      
+      self.health += current_shield_strength
+    end
+    
+    self.save!
+  end
+  
+  def dead?
+    health > 0
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -4,8 +4,56 @@ class Room < ActiveRecord::Base
   belongs_to :sector
   
   def push_button(letter)
-  #  TODO make this do something
-    "alert('dumb');"
+    letter == 'A' ? push_a_button : push_b_button
+  end
+  
+  def push_a_button
+    if upper?
+      if sector.lower_room.power > 0
+        if sector.red?
+          puts sector.enemies
+          target = sector.enemies.sort_by(&:distance).first
+          puts "Target is #{target.nil? ? "nil" : "not nil"}"
+          target.damage!(4)
+          sector.use_power!(1)
+        elsif sector.white?
+          target = sector.enemies.sort_by(&:distance).first
+          target.damage! 5
+          sector.use_power! 1          
+        else
+          target = sector.enemies.sort_by(&:distance).first
+          target.damage! 4
+          sector.use_power! 1
+        end
+        return true
+      else
+        return false
+      end
+      
+    else
+      if sector.red?
+        target = sector.enemies.sort_by(&:distance).first
+        target.health -= 2
+        target.save!
+        return true
+      elsif sector.white?
+        targets = sector.ship.sectors.map{|s| s.enemies.sort_by(&:distance).first }
+        targets.select{|t| t.distance < 8}.each do |target|
+          target.damage! 1   
+        end
+        sector.use_power! 1
+        return true
+      else
+        target = sector.enemies.sort_by(&:distance).first
+        target.health -= 2
+        target.save!
+        return true
+      end
+    end
+  end
+  
+  def push_b_button
+    
   end
   
   def location
