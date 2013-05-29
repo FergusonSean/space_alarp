588490e5a979c2280e1a5c9e6cb5f3b7765f0d8e
Sean Ferguson <sean.ferguson@appfolio.com>
2013-05-28 22:37:11 -0700

    sf fix scheduling to make actions happen on the minute

--- app/controllers/enemies_controller.rb
+++ app/controllers/enemies_controller.rb
@@ -1,7 +1,7 @@
 class EnemiesController < ApplicationController
   def create
     @enemy = Enemy.create!(params[:enemy].merge({
-      :name => (params[:health] > 8 ? 'REALLY BAD GUY' : 'bad guy'),
+      :name => (params[:enemy][:health].to_i > 8 ? 'REALLY BAD GUY' : 'bad guy'),
       :sector => Sector.find(params[:enemy][:sector_id]),
       :enemy_actions_attributes => [
         {
