d60bc95efe919a89ece788c69f89fe73f82ce5bc
Sean Ferguson <sean.ferguson@appfolio.com>
2014-07-26 19:45:53 -0700

    sf make base work in console

--- models/base.js
+++ models/base.js
@@ -1,5 +1,5 @@
 
-var add_sector(base, name) {
+var add_sector = function(base, name) {
   var upper_room = {};
   var lower_room = {};
   var sector = {};
@@ -9,6 +9,7 @@ var add_sector(base, name) {
   upper_room.sector = sector;
   lower_room.sector = sector;
 
+  console.log('adding sector');
   base[name + "_upper"] = upper_room;
   base[name + "_lower"] = lower_room;
   base[name] = sector;
@@ -20,18 +21,20 @@ var add_sector(base, name) {
 global.base = {
   initialize: function() {
     sectors = [];
-    add_sector("red");
-    add_sector("white");
-    add_sector("blue");
+    add_sector(this, "red");
+    add_sector(this, "white");
+    add_sector(this, "blue");
 
-    white_lower.batt_capacity = 5;
-    white_lower.batt = 3;
-    white_lower.cannisters = 3;
-    white_lower.btna = function() {
+    console.log('done with sectors');
+    console.log(this);
+    this.white_lower.batt_capacity = 5;
+    this.white_lower.batt = 3;
+    this.white_lower.cannisters = 3;
+    this.white_lower.btna = function() {
       console.log("pulse");
     }
     
-    white_upper.btna = function() {
+    this.white_upper.btna = function() {
       console.log("gun");
     }
   }
