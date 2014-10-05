7841210b3ebcb70ea7b16a1528b04567c8bb4a05
Sean Ferguson <sean.ferguson@appfolio.com>
2014-10-04 20:12:07 -0700

    sf add methods for changing state of ship

--- models/mission.js
+++ models/mission.js
@@ -1,4 +1,6 @@
-var ship = {
+exports._data = data 
+  
+var data = {
   upper_red: {capacity: 2, charge:1},
   lower_red: {capacity: 3, charge:2},
   upper_white: {capacity: 3, charge:1},
@@ -10,6 +12,32 @@ var ship = {
   blue:{enemies:[], damage:0}
 }
 
+exports.initialize = function() {
+  data = {
+    upper_red: {capacity: 2, charge:1},
+    lower_red: {capacity: 3, charge:2},
+    upper_white: {capacity: 3, charge:1},
+    lower_white: {capacity: 5, charge:3, cannisters:3},
+    upper_blue: {capacity: 2, charge:1},
+    lower_blue: {capacity: 3, charge:2},
+    white:{enemies:[], damage:0},
+    red:{enemies:[], damage:0},
+    blue:{enemies:[], damage:0}
+  }
+};
+
 exports.mission_data = function() {
-  return JSON.stringify(ship);
-}
+  return JSON.stringify(data);
+};
+
+exports.dealDamage = function(red, white, blue) {
+  data.red.damage += red
+  data.white.damage += white
+  data.blue.damage += blue
+};
+
+exports.movePower = function(red, white, blue) {
+  data.lower_red.charge += red
+  data.lower_white.charge += white
+  data.lower_blue.charge += blue
+};
--- test/mission.js
+++ test/mission.js
@@ -1,8 +1,8 @@
 var mission = require('../models/mission.js')
+
 exports['initialize'] = function (test) {
     test.equal(4, 4);
-    console.log(mission);
-    var data = mission.mission_data();
+    var real_data = mission.mission_data();
     expected = {
       upper_red: {capacity: 2, charge:1},
       lower_red: {capacity: 3, charge:2},
@@ -14,6 +14,65 @@ exports['initialize'] = function (test) {
       red:{enemies:[], damage:0},
       blue:{enemies:[], damage:0}
     }
-    test.deepEqual(expected, JSON.parse(data))
+    test.deepEqual(expected, JSON.parse(real_data))
     test.done();
+};
+
+exports['dealDamage'] = function (test) {
+  mission.initialize();
+  mission.dealDamage(3,2,1)
+  var real_data = mission.mission_data();
+  expected = {
+    upper_red: {capacity: 2, charge:1},
+    lower_red: {capacity: 3, charge:2},
+    upper_white: {capacity: 3, charge:1},
+    lower_white: {capacity: 5, charge:3, cannisters:3},
+    upper_blue: {capacity: 2, charge:1},
+    lower_blue: {capacity: 3, charge:2},
+    white:{enemies:[], damage:2},
+    red:{enemies:[], damage:3},
+    blue:{enemies:[], damage:1}
+  }
+  test.deepEqual(expected, JSON.parse(real_data))
+  test.done();
+};
+
+exports['initialize'] = function(test) {
+  mission.dealDamage(3,2,1);
+  mission.movePower(1,-1,1);
+  mission.initialize();
+
+  var real_data = mission.mission_data();
+  expected = {
+    upper_red: {capacity: 2, charge:1},
+    lower_red: {capacity: 3, charge:2},
+    upper_white: {capacity: 3, charge:1},
+    lower_white: {capacity: 5, charge:3, cannisters:3},
+    upper_blue: {capacity: 2, charge:1},
+    lower_blue: {capacity: 3, charge:2},
+    white:{enemies:[], damage:0},
+    red:{enemies:[], damage:0},
+    blue:{enemies:[], damage:0}
+  }
+  test.deepEqual(expected, JSON.parse(real_data))
+  test.done();
+}
+
+exports['movePower'] = function (test) {
+  mission.initialize();
+  mission.movePower(1,-1,1)
+  var real_data = mission.mission_data();
+  expected = {
+    upper_red: {capacity: 2, charge:1},
+    lower_red: {capacity: 3, charge:3},
+    upper_white: {capacity: 3, charge:1},
+    lower_white: {capacity: 5, charge:2, cannisters:3},
+    upper_blue: {capacity: 2, charge:1},
+    lower_blue: {capacity: 3, charge:3},
+    white:{enemies:[], damage:0},
+    red:{enemies:[], damage:0},
+    blue:{enemies:[], damage:0}
+  }
+  test.deepEqual(expected, JSON.parse(real_data))
+  test.done();
 };
